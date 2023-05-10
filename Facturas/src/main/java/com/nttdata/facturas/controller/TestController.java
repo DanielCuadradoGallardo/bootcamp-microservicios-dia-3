d9bc8f6666bb609498a2ab0ad3b86021f004b6f5
Daniel Cuadrado Gallardo <danielcuadradogallardo@cdpjosecabrera.es>
2023-05-10 12:46:58 +0200

    Segundo reto realizado
    Segundo reto realizado, he implementado tres microservicios (inventario, factura, personal) y le he aplicado 3 prefilters y 3 postfilters, y he establecido un gateway para acceder a los diferentes micros mediante el mismo port.

---
+++ Facturas/src/main/java/com/nttdata/facturas/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.nttdata.facturas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/")
+	public String facturas() {
+		return "facturas";
+	}
+	
+	
+}
